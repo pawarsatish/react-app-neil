9207034275d86b896bbdd33fbbfe6c3e80b96a69
Satish Pawar <Satish.Pawar@harbingergroup.com>
2019-09-05 15:32:32 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/layouts/App';
 import * as serviceWorker from './serviceWorker';
+
+
+const rootEl = document.getElementById('root');
+let render = () => {
+    ReactDOM.render(<App />,rootEl);
+}
+
+if(module.hot){
+    module.hot.accept('./app/layouts/App', () => {
+        setTimeout(render);
+    })
+}
+
+render();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
